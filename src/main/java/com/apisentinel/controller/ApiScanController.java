d6f8a850987ecea1cdef15e23647fc2ce1cb0f72
Sachinb1433 <sachinbabannavar31@gmail.com>
2025-07-30 12:16:18 +0530

    Initial commit for APISentinel backend

--- src/main/java/com/apisentinel/controller/ApiScanController.java
+++ src/main/java/com/apisentinel/controller/ApiScanController.java
@@ -1,33 +1,22 @@
 package com.apisentinel.controller;
 
 import com.apisentinel.service.OpenAPISecurityAnalyzer;
-
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "*")
 public class ApiScanController {
 
-	@PostMapping(value = "/scan-file", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-	public ResponseEntity<String> scanRawFile(@RequestParam("file") MultipartFile file) throws IOException {
-	    String rawSpec = new String(file.getBytes(), StandardCharsets.UTF_8);
-	    double score = OpenAPISecurityAnalyzer.calculateRiskScore(rawSpec);
+    @PostMapping("/scan")
+    public Map<String, Object> scanRawSpec(@RequestBody Map<String, String> body) {
+        String spec = body.get("rawSpec");
+        double score = OpenAPISecurityAnalyzer.calculateRiskScore(spec);
 
-	    Map<String, Object> result = new HashMap<>();
-	    result.put("riskScore", score);
-	    result.put("status", score > 60 ? "fail" : "pass");
-//	    return result;
-	    return ResponseEntity.ok("File uploaded");
-
-	}
-
+        Map<String, Object> response = new HashMap<>();
+        response.put("riskScore", score);
+        response.put("status", score > 60 ? "fail" : "pass");
+        return response;
+    }
 }
